39f0b555d639c92008fcdc70cdcaaf6caa8471ec
Diogo Azevedo <idiogoazevedoo@gmail.com>
2025-02-25 13:59:47 +0000

    feat: add csrf support (commented for dev)

--- internal/api/auth.go
+++ internal/api/auth.go
@@ -4,7 +4,15 @@ import (
 	"net/http"
 
 	"github.com/diogoazevedoo/go-bid/internal/jsonutils"
+	"github.com/gorilla/csrf"
 )
+
+func (api *Api) HandleGetCSRFToken(w http.ResponseWriter, r *http.Request) {
+	token := csrf.Token(r)
+	jsonutils.EncodeJson(w, r, http.StatusOK, map[string]any{
+		"csrf_token": token,
+	})
+}
 
 func (api *Api) AuthMiddleware(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
